d242dae85e662133c5d7c37f35226af09c19e440
Gabriel de Oliveira Cardoso <108769102+Cardosogoc@users.noreply.github.com>
2024-08-13 13:13:57 +0000

    Comit para o 1º deploy

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home(){
+   
+    return <div>
+                
+                <h1>Oi meu Amor!</h1>
+                <h3>testando um arquivo</h3> <h2>LOVE UUUUU</h2>
+            </div>
+    
+    
+}
+
+export default Home;
