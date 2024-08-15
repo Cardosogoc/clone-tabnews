aead8a713dc8d25b71fa4a176a83a147eaf5e636
Gabriel de Oliveira Cardoso <108769102+Cardosogoc@users.noreply.github.com>
2024-08-15 23:59:28 +0000

    Frase atualizada

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
    
     return <div>
-               <h1>Fazendo Algumas mudanças no codigo!</h1>
+               <h1>Fazendo Algumas mudanças no codigo! :D </h1>
             </div>
     
     
