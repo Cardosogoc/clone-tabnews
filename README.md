fa36c3b67e3b11ebb6f9b7ebdd17d2605c905479
Gabriel de Oliveira Cardoso <108769102+Cardosogoc@users.noreply.github.com>
2024-08-20 19:59:29 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do TabNews, estou realizando o curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-function Home(){
-   
-    return <div>
-               <h1>Fazendo Algumas mudanças no codigo! :D </h1>
-            </div>
-    
-    
+function Home() {
+  return (
+    <div>
+      <h1>Fazendo Algumas mudanças no codigo! :D </h1>
+    </div>
+  );
 }
 
 export default Home;
